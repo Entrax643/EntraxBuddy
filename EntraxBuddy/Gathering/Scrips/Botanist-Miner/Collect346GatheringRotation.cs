f0f9d10a99cb7258ae4a8b6027ded140b205b375
Entrax <entrax.643@gmail.com>
2016-01-02 00:21:57 +0100

    Charizard version

---
+++ EntraxBuddy/Gathering/Scrips/Botanist-Miner/Collect346GatheringRotation.cs
@@ -0,0 +1,62 @@
+﻿namespace ExBuddy.OrderBotTags.Gather.Rotations
+{
+	using System.Threading.Tasks;
+
+	using ExBuddy.Attributes;
+	using ExBuddy.Interfaces;
+
+	using ff14bot;
+	[GatheringRotation("Collect346", 30, 600,400,200)]
+	public sealed class Collect346GatheringRotation : CollectableGatheringRotation, IGetOverridePriority
+	{
+		#region IGetOverridePriority Members
+		int IGetOverridePriority.GetOverridePriority(ExGatherTag tag)
+		{
+			// if we have a collectable && the collectable value is greater than or equal to 346: Priority 346
+			if (tag.CollectableItem != null && tag.CollectableItem.Value >= 346)
+			{
+				return 346;
+			}
+			return -1;
+		}
+		#endregion
+		public override async Task<bool> ExecuteRotation(ExGatherTag tag)
+		{
+			if (tag.IsUnspoiled())
+			{
+				await SingleMindMethodical(tag);
+				await SingleMindMethodical(tag);
+				await SingleMindMethodical(tag);
+			}
+			else
+			{
+				if (Core.Player.CurrentGP >= 600)
+				{
+					await SingleMindMethodical(tag);
+					await SingleMindMethodical(tag);
+					await SingleMindMethodical(tag);
+					return true;
+				}
+				if (Core.Player.CurrentGP >= 400)
+				{
+					await Methodical(tag);
+					await SingleMindMethodical(tag);
+					await SingleMindMethodical(tag);
+					return true;
+				}
+				if (Core.Player.CurrentGP >= 200)
+				{
+					await Methodical(tag);
+					await Methodical(tag);
+					await SingleMindMethodical(tag);
+					return true;
+				}
+				
+				await Methodical(tag);
+				await Methodical(tag);
+				await Methodical(tag);
+			}
+			return true;
+		}
+	}
+}
---
+++ EntraxBuddy/Gathering/Scrips/Botanist-Miner/Collect460GatheringRotation.cs
@@ -0,0 +1,55 @@
+﻿namespace ExBuddy.OrderBotTags.Gather.Rotations
+{
+	using System.Threading.Tasks;
+
+	using ExBuddy.Attributes;
+	using ExBuddy.Interfaces;
+
+	using ff14bot;
+	// Name, Time, Get3, Get2, Get1, Get0+
+	[GatheringRotation("Collect460", 33, 600)]
+	public sealed class Collect460GatheringRotation : CollectableGatheringRotation, IGetOverridePriority
+	{
+		#region IGetOverridePriority Members
+		int IGetOverridePriority.GetOverridePriority(ExGatherTag tag)
+		{
+			// if we have a collectable && the collectable value is greater than or equal to 460: Priority 460
+			if (tag.CollectableItem != null && tag.CollectableItem.Value >= 460)
+			{
+				return 460;
+			}
+			return -1;
+		}
+		#endregion
+		public override async Task<bool> ExecuteRotation(ExGatherTag tag)
+		{
+			if (tag.IsUnspoiled())
+			{
+				await SingleMindMethodical(tag);
+				await SingleMindMethodical(tag);
+				await UtmostCaution(tag);
+				await Methodical(tag);
+				await UtmostCaution(tag);
+				await Methodical(tag);
+			}
+			else
+			{
+				if (tag.GatherItem.Chance < 98 && Core.Player.CurrentGP >= 600)
+				{
+					await SingleMindMethodical(tag);
+					await SingleMindMethodical(tag);
+					await UtmostCaution(tag);
+					await Methodical(tag);
+					await UtmostCaution(tag);
+					await Methodical(tag);
+					return true;
+				}
+				
+				await Impulsive(tag);
+				await Impulsive(tag);
+				await Methodical(tag);
+			}
+			return true;
+		}
+	}
+}
